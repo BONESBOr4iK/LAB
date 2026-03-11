6e6965c1d6442f0c05e982a07d6e2e22eabae588
_Bor4iK_ <shafiev06@mail.ru>
2026-03-11 15:33:50 +0500

    Закончил работу

--- lab 9-10/Viruses.cpp
+++ lab 9-10/Viruses.cpp
@@ -12,6 +12,7 @@ protected:
     bool VaccineIsTrue; // Есть ли вакцина
 
 public:
+    std::string GetCarrier() const { return Carrier; }; // Геттер
     Virus(double death_percent, std::string carrier, unsigned int count_victims, bool vaccineIsTrue);
     virtual ~Virus();
     virtual double GetDeath_percent() const { return Death_percent; };
@@ -142,16 +143,104 @@ void EX201000CF::Print() const
         std::cout << "Вакцина не была создана, эпидемия продолжается" << std::endl;
 }
 
+class Umbrella : public Virus
+{
+public:
+    Umbrella(std::string carrier);
+    ~Umbrella();
+    void Print() const override;
+    int GetCount_victims() const override;
+    double GetDeath_percent() const override;
+    bool Win() const override;
+    double Zombie_percent() const;
+};
+
+Umbrella::Umbrella(std::string carrier)
+    : Virus(95.0, carrier, 5500000000, false)
+{
+    std::cout << "Выпуск вируса в Racoon city" << std::endl;
+}
+
+Umbrella::~Umbrella()
+{
+    std::cout << "Вирус выбрался за пределы" << std::endl;
+}
+
+int Umbrella::GetCount_victims() const
+{
+    return Virus::GetCount_victims();
+}
+
+double Umbrella::GetDeath_percent() const
+{
+    return Virus::GetDeath_percent();
+}
+
+bool Umbrella::Win() const
+{
+    return Virus::Win();
+}
+
+double Umbrella::Zombie_percent() const
+{
+    std::string carrier = GetCarrier();
+    if (carrier == "Human")
+    {
+        std::cout << " Высокий риск зомбификации" << std::endl;
+        return 97.0;
+    }
+    else if (carrier == "Pet")
+    {
+        std::cout << " Большой риск зомбификации" << std::endl;
+        return 76.0;
+    }
+    else if (carrier == "Rat")
+    {
+        std::cout << " Средний риск зомбификации" << std::endl;
+        return 55.0;
+    }
+    else if (carrier == "Bird")
+    {
+        std::cout << " Низкий риск зомбификации" << std::endl;
+        return 25.0;
+    }
+    else
+    {
+        std::cout << " Неизвестный носитель, информация неизвестна. Риск зомбификации" << std::endl;
+        return GetDeath_percent() * 0.5;
+        ;
+    }
+}
+void Umbrella::Print() const
+{
+    Virus::Print();
+    std::cout << "Переносчик: " << GetCarrier() << Zombie_percent() << "%" << std::endl;
+    if (Win())
+    {
+        std::cout << "Вирус побежден, с помощью вакцины: " << std::endl;
+    }
+    else
+        std::cout << "Вакцина не была создана, эпидемия продолжается" << std::endl;
+}
+
 int main()
 {
     {
-        Plague Bubonic_Plague;
-        Bubonic_Plague.Print();
+        Virus *NewVirus1 = new Plague;
+        NewVirus1->Print();
+        delete NewVirus1;
     }
     std::cout << "=---------------------------------------------------=" << std::endl;
     {
-        EX201000CF NewStamm;
-        NewStamm.Print();
+        Virus *NewVirus2 = new EX201000CF;
+        NewVirus2->Print();
+        delete NewVirus2;
+    }
+    std::cout << "=---------------------------------------------------=" << std::endl;
+    {
+        Virus *T_Virus = new Umbrella("Pigeon");
+        T_Virus->Print();
+        delete T_Virus;
     }
     return 0;
 }
